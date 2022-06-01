435002c8c824d3b2f5301fc3c75e3eee5f29506f
rohit kumar <rohit.kumar@sprinklr.com>
2022-06-01 21:13:21 +0530

    Constants updated

--- src/extension/domQueries.ts
+++ src/extension/domQueries.ts
@@ -1,14 +1,19 @@
-import { STATUS_BADGE_MERGED } from './constants';
+import { STATUS_BADGE_MERGED,DETAIL_PAGE_DESCRIPTION } from './constants';
 
 function isAlreadyMerged() {
   return !!document.querySelector(STATUS_BADGE_MERGED);
 }
 
 function getSourceBranch() {
-  const el = document.querySelector(".detail-page-description").getElementsByTagName('a')[1].href.toString();
-    const element = el.split('/').at(-1);
+    // Use for Free Tier Gitlab
+    const el = document.querySelector(DETAIL_PAGE_DESCRIPTION).getElementsByTagName('a')[1].href.toString(); 
+    const element = el.split('/').at(-1);    
+
+    // Use for Prod-Gitlab
+    // const el = document.querySelector(".js-source-branch");
+    // const element = el.textContent?.trim();
+
     if (element) {
-      // return el.textContent?.trim();
       return element;
     } else {
       return void 0;
@@ -16,10 +21,15 @@ function getSourceBranch() {
 }
 
 function getTargetBranch() {
-  const el = document.querySelector(".detail-page-description").getElementsByTagName('a')[2].href.toString();
-    const element = el.split('/').at(-1);
+    // Use for Free Tier Gitlab
+    const el = document.querySelector(DETAIL_PAGE_DESCRIPTION).getElementsByTagName('a')[2].href.toString(); 
+    const element = el.split('/').at(-1);    
+
+    // Use for Prod-Gitlab
+    // const el = document.querySelector(".js-target-branch");
+    // const element = el.textContent?.trim();
+
     if (element) {
-      // return el.textContent?.trim();
       return element;
     } else {
       return void 0;
